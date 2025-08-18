f79a270179186a86a351495690d760dfd7c52dbe
Marco Sorbi <Marco.Sorbi@unige.ch>
2025-08-18 10:27:39 +0200

    multiple dictionary sizes

--- scripts/run_hdsner.sh
+++ scripts/run_hdsner.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
-if [ $# -ne 1 ] || ([ "$1" != "supervised" ] && [ "$1" != "distant" ])
-then
-    echo "usage: $0 (supervised|distant)"
-    exit 1
-fi
-setting="$1"
-
-source="hdsner-utils/data/${setting}/ner_medieval_multilingual/FR/"
 output_dir="dataset"
 dataset_prefix="hdsner-"
-dataset_suffix="_${setting}"
+for setting in `ls "hdsner-utils/data/"`
+do
+    source="hdsner-utils/data/${setting}/ner_medieval_multilingual/FR/"
+    if [ ! -d "${source}" ] || [ "${setting}" = "data_raw" ]
+    then
+        continue
+    fi
+    if [ "${setting}" = "supervised" ]
+    then
+        dataset_suffix="_Fully"
+    else
+        p=`echo "${setting}" | cut -d '-' -f 2`
+        dataset_suffix="_Dict${p}"
+    fi
 
-# copy and format datasets
-python3 scripts/format_hdsner_datasets.py \
-    --input-dir "${source}" \
-    --output-dir "${output_dir}" \
-    "--output-prefix=${dataset_prefix}" \
-    "--output-suffix=${dataset_suffix}"
+    # copy and format datasets
+    python3 scripts/format_hdsner_datasets.py \
+        --input-dir "${source}" \
+        --output-dir "${output_dir}" \
+        "--output-prefix=${dataset_prefix}" \
+        "--output-suffix=${dataset_suffix}"
+done
 
 # execute on all datasets
-for dataset in ${output_dir}/${dataset_prefix}*${dataset_suffix}
+for dataset in ${output_dir}/${dataset_prefix}*
 do
     dataset_name="`basename ${dataset}`"
     time \
